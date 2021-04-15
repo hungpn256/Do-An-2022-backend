f7c455f07e9f4f7f67bf70fad54295ee4e450201
khanhnd-it24 <khanhnd.it24@gmail.com>
2021-04-15 16:13:26 +0700

    update get posts and create post

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -182,12 +182,12 @@ router.get('/:userId?', async (req,res) => {
         error: err
       });
     }
-    if(posts.length === 0) {
-      return res.status(400).json({
-        success: false,
-        posts: []
-      });
-    }
+    // if(posts.length === 0) {
+    //   return res.status(400).json({
+    //     success: false,
+    //     posts: []
+    //   });
+    // }
     return res.status(200).json({
       success: true,
       posts: posts
