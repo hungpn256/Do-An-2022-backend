a5bde1031333b025506516938849bf60035c37a7
Hung Pham <phamnanghung.25@gmail.com>
2021-04-22 17:20:26 +0700

    access key khanhadmin

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,21 +1,22 @@
 module.exports = {
-    port: process.env.PORT || 5000,
-    database: {
-        url: process.env.MONGO_URI
-    },
-    jwt: {
-        secret: process.env.JWT_SECRET,
-        tokenLife: '7d'
-    },
-    googleapis: {
-        CLIENT_ID: "1074375836033-k38tq2e4qcgnh8lls44d6oc0ehmbslku.apps.googleusercontent.com",
-        CLIENT_SECRET: "hxCFoCMpT4KaNsongclzt6gg",
-        REDIRECT_URI: "https://developers.google.com/oauthplayground",
-        REFRESH_TOKEN: "1//04E13S0-qAi7iCgYIARAAGAQSNwF-L9Iru62Eluga5dl2p-B8aohXs4S7i64Xn8aQ0JvjHeGaoJ7Sfs5bFgFGCL3rW6cEC0QnpG8"
-
-    },
-    aws: {
-        secretKey: "VfaGrhprt5YwrrjF/kU0Ehr2QkdzG5EHvQYWOSN4",
-        accessKey: "AKIAYDX3CKBH2CMWPNVB"
-    }
-}
+  port: process.env.PORT || 5000,
+  database: {
+    url: process.env.MONGO_URI,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    tokenLife: '7d',
+  },
+  googleapis: {
+    CLIENT_ID:
+      '1074375836033-k38tq2e4qcgnh8lls44d6oc0ehmbslku.apps.googleusercontent.com',
+    CLIENT_SECRET: 'hxCFoCMpT4KaNsongclzt6gg',
+    REDIRECT_URI: 'https://developers.google.com/oauthplayground',
+    REFRESH_TOKEN:
+      '1//04E13S0-qAi7iCgYIARAAGAQSNwF-L9Iru62Eluga5dl2p-B8aohXs4S7i64Xn8aQ0JvjHeGaoJ7Sfs5bFgFGCL3rW6cEC0QnpG8',
+  },
+  aws: {
+    secretKey: 'AYOsaGU0Rb2m1Vt7NFfR1N/e3nKM2RNYQmuvHT2h',
+    accessKey: 'AKIAYDX3CKBH35Q2V4XD',
+  },
+};
