b828ff440f33222b998204bc65bedc95408aab58
Khanh Nguyen <khanhnd.it24@gmail.com>
2021-04-28 11:24:41 +0700

    update api create post

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -4,9 +4,11 @@ const authRoutes = require('./auth.js');
 const userRoutes = require('./user.js');
 const postRoutes = require('./post.js');
 const imageUploadRoutes = require('./file-upload.js');
-const { queryVar } = require('../../services/query.js');
+const {queryVarUser, queryVarPost } = require('../../services/query.js');
 const User = require('../../models/user.js');
 const Post = require('../../models/post.js');
+const { removeAccents } = require('../../helps/removeAccent.js');
+
 // auth routes
 router.use('/auth', authRoutes);
 
@@ -21,13 +23,43 @@ router.use('/images', imageUploadRoutes);
 
 router.get('/search', async (req,res) => {
   let q = req.query.q;
-  q = queryVar(q);
-  console.log(q);
-  User.find(q).then(result => {
-    res.json(result);
+  let result = {};
+  const qUser = queryVarUser(removeAccents(q));
+  console.log(qUser);
+  await User.find(qUser).then( users => {
+    const _users = users.map(v => {
+      return {
+        _id: v._id,
+        name: {
+          firstName: v.firstName,
+          lastName: v.lastName,
+        },
+        avatar: v.avatar
+      }
+    });
+    result.users = _users;
   }).catch(err => {
-    return res.json({err});
+    return res.status(400).json({
+      success: false,
+      message:
+        'Your request could not be processed. Please try again.',
+    });
   });
 
+  const qPost = queryVarPost(removeAccents(q));
+  console.log(qPost);
+  await Post.find(qPost).then(posts => {
+    result.posts = posts;
+  }).catch(err => {
+    return res.status(400).json({
+      success: false,
+      message:
+        'Your request could not be processed. Please try again.',
+    });
+  });
+
+  return res.status(200).json({
+    result
+  })
 })
 module.exports = router;
--- src/services/query.js
+++ src/services/query.js
@@ -1,6 +1,6 @@
 const stopwords = require('vietnamese-stopwords');
 
-module.exports.queryVar = function(str) {
+module.exports.queryVarUser = function(str) {
     let q = str.replace( /\r\n/g, '').replace(/^\s+|\s+$/, '').replace(/[^a-z\s]+/gi, '').replace(/\s+$/, '');
 
     let parts = q.split(/\s/);
@@ -13,6 +13,23 @@ module.exports.queryVar = function(str) {
     let query = {'$and': []};
     terms.forEach(term => {
        let queryFrag = {fullName: {'$regex': term, '$options': 'i'}};
+       query['$and'].push(queryFrag);
+    });
+    return query;
+};
+module.exports.queryVarPost = function(str) {
+    let q = str.replace( /\r\n/g, '').replace(/^\s+|\s+$/, '').replace(/[^a-z\s]+/gi, '').replace(/\s+$/, '');
+
+    let parts = q.split(/\s/);
+    let terms = [];
+    parts.forEach(part => {
+        if(stopwords.indexOf(part) === -1) {
+            terms.push(part);
+        }
+    });
+    let query = {'$and': []};
+    terms.forEach(term => {
+       let queryFrag = {textAccent: {'$regex': term, '$options': 'i'}};
        query['$and'].push(queryFrag);
     });
     return query;
