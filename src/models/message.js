e6bd4f99eec33bceec998bb8458c2c00f21211e4
hung <phamnanghung.25@gmail.com>
2022-09-08 15:26:16 +0700

    message

--- src/models/message.js
+++ src/models/message.js
@@ -1,35 +1,37 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    content: {
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  content: {
+    type: String,
+  },
+  files: [
+    {
+      url: String,
+      typeMedia: {
         type: String,
+        enum: ["IMAGE", "VIDEO"],
+      },
     },
-    files: [{
-        url: String,
-        typeMedia: {
-            type: String,
-            enum: ["IMAGE", "VIDEO"],
-        },
-    }],
-    reply: {
-        type: mongoose.Schema.Types.ObjectId,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now
-    },
-    conversationId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Conversation"
-    },
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
+  ],
+  reply: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  conversation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Conversation",
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 module.exports = mongoose.model("Message", messageSchema);
--- src/routes/api/conversation.js
+++ src/routes/api/conversation.js
@@ -4,6 +4,7 @@ const User = require("../../models/user.js");
 const Comment = require("../../models/comment.js");
 const Like = require("../../models/like.js");
 const Conversation = require("../../models/conversation");
+const Message = require("../../models/message");
 
 const { requireSignin } = require("../../middleware/index.js");
 const mongoose = require("mongoose");
@@ -60,14 +61,76 @@ router.post("/", requireSignin, async (req, res) => {
 
 router.get("/", requireSignin, async (req, res) => {
   try {
+    const limit = req.query.limit || 10;
+    const lastConversationId = req.query.lastConversationId;
     const user = req.user;
     const { _id } = user;
+    const query = {
+      _id: { $lt: lastConversationId },
+      "participants.user": _id,
+    };
 
-    const conversations = await Conversation.find({ participants: _id });
+    if (!lastConversationId) {
+      delete query._id;
+    }
+
+    const conversations = await Conversation.find(query)
+      .sort({ updatedAt: 1 })
+      .limit(limit);
 
     return res.status(200).json({
       success: true,
       conversations: conversations,
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      message: "Some thing went wrong",
+      error: e,
+    });
+  }
+});
+
+router.get("/message/:conversationId", requireSignin, async (req, res) => {
+  try {
+    const limit = req.query.limit || 10;
+    const lastMessageId = req.query.lastMessageId;
+    const conversationId = req.params.conversationId;
+    const query = {
+      _id: { $lt: lastMessageId },
+      conversation: conversationId,
+    };
+    if (!lastMessageId) {
+      delete query._id;
+    }
+
+    const messages = await Message.find(query)
+      .sort({ createdAt: 1 })
+      .limit(limit);
+
+    return res.status(200).json({
+      success: true,
+      messages: messages,
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      message: "Some thing went wrong",
+      error: e,
+    });
+  }
+});
+
+router.post("/message", requireSignin, async (req, res) => {
+  try {
+    const message = req.body.message;
+    message.createdBy = req.user._id;
+
+    const newMessages = new Message(message);
+    const messageSave = await newMessages.save();
+    return res.status(200).json({
+      success: true,
+      message: messageSave,
     });
   } catch (e) {
     return res.status(400).json({
