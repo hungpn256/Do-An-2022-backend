5e17d17cef804ef778802dc221a9d385a0bb61d1
hung <phamnanghung.25@gmail.com>
2022-10-03 16:22:28 +0700

    query

--- src/services/query.js
+++ src/services/query.js
@@ -1,36 +1,44 @@
-const stopwords = require('vietnamese-stopwords');
+const stopwords = require("vietnamese-stopwords");
 
-module.exports.queryVarUser = function(str) {
-    let q = str.replace( /\r\n/g, '').replace(/^\s+|\s+$/, '').replace(/[^a-z\s]+/gi, '').replace(/\s+$/, '');
+module.exports.queryVarUser = function (str) {
+  let q = str
+    .replace(/\r\n/g, "")
+    .replace(/^\s+|\s+$/, "")
+    .replace(/[^a-z\s]+/gi, "")
+    .replace(/\s+$/, "");
 
-    let parts = q.split(/\s/);
-    let terms = [];
-    parts.forEach(part => {
-        if(stopwords.indexOf(part) === -1) {
-            terms.push(part);
-        }
-    });
-    let query = {'$and': []};
-    terms.forEach(term => {
-       let queryFrag = {fullName: {'$regex': term, '$options': 'i'}};
-       query['$and'].push(queryFrag);
-    });
-    return query;
+  let parts = q.split(/\s/);
+  let terms = [];
+  parts.forEach((part) => {
+    if (stopwords.indexOf(part) === -1) {
+      terms.push(part);
+    }
+  });
+  let query = { $and: [] };
+  terms.forEach((term) => {
+    let queryFrag = { fullName: { $regex: term, $options: "i" } };
+    query["$and"].push(queryFrag);
+  });
+  return query;
 };
-module.exports.queryVarPost = function(str) {
-    let q = str.replace( /\r\n/g, '').replace(/^\s+|\s+$/, '').replace(/[^a-z\s]+/gi, '').replace(/\s+$/, '');
+module.exports.queryVarPost = function (str) {
+  let q = str
+    .replace(/\r\n/g, "")
+    .replace(/^\s+|\s+$/, "")
+    .replace(/[^a-z\s]+/gi, "")
+    .replace(/\s+$/, "");
 
-    let parts = q.split(/\s/);
-    let terms = [];
-    parts.forEach(part => {
-        if(stopwords.indexOf(part) === -1) {
-            terms.push(part);
-        }
-    });
-    let query = {'$and': []};
-    terms.forEach(term => {
-       let queryFrag = {textAccent: {'$regex': term, '$options': 'i'}};
-       query['$and'].push(queryFrag);
-    });
-    return query;
+  let parts = q.split(/\s/);
+  let terms = [];
+  parts.forEach((part) => {
+    if (stopwords.indexOf(part) === -1) {
+      terms.push(part);
+    }
+  });
+  let query = { $and: [] };
+  terms.forEach((term) => {
+    let queryFrag = { text: { $regex: term, $options: "i" } };
+    query["$and"].push(queryFrag);
+  });
+  return query;
 };
